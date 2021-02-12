6b78944d5a1291a16d4ef52a516a43b151243978
LucianoSm20 <luciano.sanmartin.h@gmail.com>
2021-02-11 21:53:47 -0300

    v2
    v2

--- app2/views.py
+++ app2/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
+import json
+import datetime
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.conf import settings
+from .forms import FormularioBusqueda
 
 # Create your views here.
 
@@ -12,14 +16,67 @@ def registro(request):
 def login(request):
     return render(request, 'app2/login.html')
 
-def fichaPaciente(request):
-    return render(request, 'app2/fichapaciente.html')
-
-def fichaMedica(request):
-    return render(request, 'app2/fichamedica.html')
-
 def calendarioHoras(request):
     return render(request, 'app2/calendarioHoras.html')
 
 def administracion(request):
     return render(request, 'app2/administracion.html')
+
+
+def fichaMedica(request):
+    formulario = FormularioBusqueda()
+    if request.method == "GET":
+        formulario = FormularioBusqueda()
+        context = {'formulario': formulario, 'invalid': False}
+        print(context)
+        return render(request, 'app2/fichamedica.html', context)
+
+    elif request.method == "POST":
+        print("El POST contiene:", request.POST)
+
+        formulario_devuelto = FormularioBusqueda(request.POST)
+        usuario_data = verificar_user(formulario_devuelto)
+
+        if formulario_devuelto.is_valid() == True and usuario_data != False:
+            context = {'usuario': usuario_data}
+            return render(request, 'app2/fichapaciente.html', context)
+        elif formulario_devuelto.is_valid() == False:
+            formulario = FormularioBusqueda()
+            context = {'formulario': formulario, 'invalid': True}
+            return render(request, 'app2/fichamedica.html', context)
+        else:
+            formulario = FormularioBusqueda()
+            context = {'formulario': formulario, 'invalid': True}
+            return render(request, 'app2/fichamedica.html', context)
+    else:
+        formulario = FormularioBusqueda()
+        context = {'formulario': formulario, 'invalid': True}
+        return render(request, 'app2/fichamedica.html', context)
+
+
+def verificar_user(run_usuario):
+    lista_usuarios = context_lista_pacientes()
+    print(type(lista_usuarios))
+    print(lista_usuarios)
+    for pacientes in lista_usuarios:
+        print('holaaaaaaaaaaaaaaaaaaaa')
+        print(pacientes)
+        if pacientes.rut == run_usuario:
+            return pacientes
+        else:
+            continue
+    return False
+
+
+def context_lista_pacientes():
+    filename= "/app2/data/pacientes.json"
+    with open(str(settings.BASE_DIR)+filename, 'r') as file:
+        pacientes=json.load(file)
+    context = {'lista_pacientes': pacientes['pacientes']}
+    return context
+
+
+def lista_pacientes(request):
+    context = context_lista_pacientes()
+    return render(request, 'app2/fichapaciente.html', context)
+
