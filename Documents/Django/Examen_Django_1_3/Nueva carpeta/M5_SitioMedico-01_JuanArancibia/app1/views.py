9f447d2687b868e266abed4eb349b7a922f9a89b
LUIS <luis.debia@gmail.com>
2021-02-12 12:54:04 -0300

    envío proyecto con pantillas_html

---
+++ Documents/Django/Examen_Django_1_3/Nueva carpeta/M5_SitioMedico-01_JuanArancibia/app1/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.template import loader, context
+from django.http import HttpResponse
+from django.http import HttpResponsePermanentRedirect
+
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'app1/index.html')
+
+def registro(request):
+    return render(request, 'app1/register.html')
+
+
+
+def login(request):
+    return render(request, 'app1/login.html')
+
+def fichaPaciente(request):
+    return render(request, 'app1/fichapaciente.html')
+
+def fichaMedica(request):
+    return render(request, 'app1/fichamedica.html')
+
+def calendarioHoras(request):
+    return render(request, 'app1/calendarioHoras.html')
+
+def administracion(request):
+    return render(request, 'app1/administracion.html')
